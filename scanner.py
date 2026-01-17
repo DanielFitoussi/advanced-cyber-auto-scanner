e52522e0c6839f3b0b7e6595f92438b63c1071e9
Daniel Fitoussi <daniel7fitoussi@gmail.com>
2026-01-17 20:42:56 +0200

    add basic idor check

--- scanner.py
+++ scanner.py
@@ -1,9 +1,12 @@
 from tools.zap_runner import run_zap_scan
+from tools.idor_check import run_idor_check
 
 
 def main():
     target_url = "http://localhost:3000"
+
     run_zap_scan(target_url)
+    run_idor_check()
 
 
 if __name__ == "__main__":
---
+++ tools/idor_check.py
@@ -0,0 +1,3 @@
+def run_idor_check():
+    print("running idor check")
+    print("trying to access object that does not belong to the user")
