cad149a063fa2b405292e9ee944f8e0d2f124d31
Daniel Fitoussi <daniel7fitoussi@gmail.com>
2026-01-17 20:39:54 +0200

    connect scanner to zap runner

--- scanner.py
+++ scanner.py
@@ -1,5 +1,9 @@
+from tools.zap_runner import run_zap_scan
+
+
 def main():
-    print("scanner started")
+    target_url = "http://localhost:3000"
+    run_zap_scan(target_url)
 
 
 if __name__ == "__main__":
---
+++ tools/zap_runner.py
@@ -0,0 +1,2 @@
+def run_zap_scan(target_url):
+    print("running zap scan on:", target_url)
