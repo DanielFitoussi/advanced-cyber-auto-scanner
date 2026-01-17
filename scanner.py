b3a4494f3813bc43d362f8770cff4934aea61a72
Daniel Fitoussi <daniel7fitoussi@gmail.com>
2026-01-17 20:48:43 +0200

    add semgrep static analysis runner

--- scanner.py
+++ scanner.py
@@ -1,7 +1,8 @@
 import sys
 from tools.zap_runner import run_zap_scan
-from tools.idor_check import run_idor_check
 from tools.nuclei_runner import run_nuclei_scan
+from tools.idor_check import run_idor_check
+from tools.semgrep_runner import run_semgrep_scan
 
 
 def main():
@@ -10,9 +11,11 @@ def main():
         return
 
     target_url = sys.argv[1]
+    target_path = "."
 
     run_zap_scan(target_url)
     run_nuclei_scan(target_url)
+    run_semgrep_scan(target_path)
     run_idor_check()
 
 
---
+++ tools/semgrep_runner.py
@@ -0,0 +1,3 @@
+def run_semgrep_scan(target_path):
+    print("running semgrep scan on path:", target_path)
+    print("checking source code for insecure patterns")
